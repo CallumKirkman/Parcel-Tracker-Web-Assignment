cf42197051615ba0c83ac6769a09ec276916545b
Callum Kirkman <56700517+CallumKirkman@users.noreply.github.com>
2021-12-04 16:28:24 +0000

    Move connection params into connection function

--- flaskr/db.py
+++ flaskr/db.py
@@ -2,13 +2,35 @@ import os
 import pymysql
 from flask import jsonify
 
-db_user = os.environ.get('CLOUD_SQL_USERNAME')
-db_password = os.environ.get('CLOUD_SQL_PASSWORD')
-db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
-db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+# # Alternate connection
+# # When deployed to App Engine, the `GAE_ENV` environment variable will be set to `standard`
+#     if os.environ.get('GAE_ENV') == 'standard':
+#         # If deployed, use the local socket interface for accessing Cloud SQL
+#         unix_socket = '/cloudsql/{}'.format(db_connection_name)
+#         cnx = pymysql.connect(user=db_user, password=db_password, unix_socket=unix_socket, db=db_name)
+#     else:
+#         # If running locally, use the TCP connections instead Set up Cloud SQL Proxy
+#         # (cloud.google.com/sql/docs/mysql/sql-proxy) so that your application can use
+#         # 127.0.0.1:3306 to connect to your cloud SQL instance
+#         host = '127.0.0.1'
+#         cnx = pymysql.connect(user=db_user, password=db_password, host=host, db=db_name)
+#
+#     with cnx.cursor() as cursor:
+#         cursor.execute('select item_name from item;')
+#         result = cursor.fetchall()
+#         current_msg = result[0][0]
+#     cnx.close()
+#
+#     # current_msg = current_msg
 
 
 def open_connection():
+    db_user = os.environ.get('CLOUD_SQL_USERNAME')
+    db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+    db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+    db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
     unix_socket = '/cloudsql/{}'.format(db_connection_name)
     try:
         if os.environ.get('GAE_ENV') == 'standard':
---
+++ flaskr/dbData.py
@@ -0,0 +1,2 @@
+def dummyData():
+    return (1, "xbox 360"), (2, "xbox one"), (3, "xbox series x")
