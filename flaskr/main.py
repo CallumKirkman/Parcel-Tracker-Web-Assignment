dc364113bc57e8a4155f14f3673c7c87460d1038
Callum Kirkman <56700517+CallumKirkman@users.noreply.github.com>
2021-12-07 17:14:40 +0000

    Re-merge database code & add requirements for gcloud app

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,11 +1,66 @@
 import logging
 import pyrebase
+import os
+import pymysql
 
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify, redirect, url_for, flash, session, abort
-from flaskr.database import open_connection, get_data, create_data
+# from flaskr.database import open_connection, get_data, create_data
 
 
 app = Flask(__name__)
+
+
+def open_connection():
+    try:
+        # When deployed to App Engine, the `GAE_ENV` environment variable will be set to `standard`
+        if os.environ.get('GAE_ENV') == 'standard':
+            db_user = os.environ.get('CLOUD_SQL_USERNAME')
+            db_password = os.environ.get('CLOUD_SQL_PASSWORD')
+            db_name = os.environ.get('CLOUD_SQL_DATABASE_NAME')
+            db_connection_name = os.environ.get('CLOUD_SQL_CONNECTION_NAME')
+
+            # If deployed, use the local socket interface for accessing Cloud SQL
+            unix_socket = '/cloudsql/{}'.format(db_connection_name)
+            cnx = pymysql.connect(user=db_user, password=db_password, unix_socket=unix_socket, db=db_name,
+                                  cursorclass=pymysql.cursors.DictCursor)
+        else:
+            load_dotenv()
+            db_user = os.getenv('CLOUD_SQL_USERNAME')
+            db_password = os.getenv('CLOUD_SQL_PASSWORD')
+            db_name = os.getenv('CLOUD_SQL_DATABASE_NAME')
+
+            # If running locally, use the TCP connections instead
+            # Set up Cloud SQL Proxy (cloud.google.com/sql/docs/mysql/sql-proxy)
+            host = '127.0.0.1'
+            port = 1433
+            cnx = pymysql.connect(user=db_user, password=db_password, host=host, port=port, db=db_name,
+                                  cursorclass=pymysql.cursors.DictCursor)
+
+    except pymysql.MySQLError as e:
+        return e
+
+    return cnx
+
+
+def get_data(cnx):
+    with cnx.cursor() as cursor:
+        result = cursor.execute('select * from item;')
+        items = cursor.fetchall()
+    if result > 0:
+        return items
+        # got_items = jsonify(items)  # Needed?
+    else:
+        got_items = 'No items in DB'
+        return got_items
+
+
+def create_data(cnx, item):
+    with cnx.cursor() as cursor:
+        cursor.execute('INSERT INTO item (item_id, item_name) VALUES(%s, %s)', (item["4"], item["test"]))
+    cnx.commit()
+    cnx.close()
+
 
 # Firebase details
 firebaseConfig = {
